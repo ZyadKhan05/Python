59d76673626b1cd0e6c11bd28cf8138509dd4b16
Zyad Khan <74572665+ZyadKhan05@users.noreply.github.com>
2023-06-04 20:56:31 -0400

    Add files via upload

---
+++ Week 7 Labs/Lab6_Prob3.py
@@ -0,0 +1,17 @@
+#Problem 3 - Create a Password
+
+def intro():
+    print('Hello, this program will collect a string from the user. Then it will take the first letter of those words to create a password')
+
+word = ''
+
+def main():
+    intro()
+    result = ''
+    word = input('Enter your sentence to create a password: ')
+    words = word.split()
+    password = [word[0] for word in words]
+    print('Your new password is:', "".join(password))
+    
+main()
+
